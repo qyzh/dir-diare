f1159f44380dc5c9fa5bf11ff64b50ba0163259a
qqy <qyzh@users.noreply.github.com>
2024-05-14 18:21:58 +0700

    create 3 files and update 4 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import { BlogPosts } from 'app/components/posts'
-
+import { SpotlightPreview } from 'app/components/dotheader'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter ">
-      Hey, I'm Qyou 👋
-      </h1>
-      <p className="prose prose-neutral dark:prose-invert">
-      Syauqi Ashadullah is my name and I live in Bandung.
-      My favourite things are <i>coffee</i> + <i>live music</i>. In short, 
-      I made <strong>this blog</strong> for me personally, because I don't like to tell people about my <strong>life</strong>, <strong>feelings</strong>, <strong>love</strong>, <strong>drama</strong>, <strong>college</strong>, <strong>friendship </strong>
-      verbally, I prefer to let it out by <a href='/blog' className="font-semibold">writing</a> it. Because if I don't express these feelings, it can make me <strong>gila</strong>.
-      </p>
+      <SpotlightPreview />
       <div className="my-8">
         <BlogPosts />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
-import type { Config } from 'tailwindcss';
-import typography from '@tailwindcss/typography';
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}', 
     './content/**/*.mdx',
     './public/**/*.svg', 
-    './components/**/*.{js,ts,jsx,tsx,mdx}'
+    '.app/components/**.{js,ts,jsx,tsx,mdx}',
   ],
     theme: {
       extend: {
+        animation: {
+          spotlight: "spotlight 2s ease .75s 1 forwards",
+        },
+        keyframes: {
+          spotlight: {
+            "0%": {
+              opacity: 0,
+              transform: "translate(-72%, -62%) scale(0.5)",
+            },
+            "100%": {
+              opacity: 1,
+              transform: "translate(-50%,-40%) scale(1)",
+            },
+          },
+        },
         fontFamily: {
           sans: ['var(--font-geist-sans)'],
           mono: ['var(--font-geist-mono)'],
