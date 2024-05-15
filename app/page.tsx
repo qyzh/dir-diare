7252584192581ce0beb0bad53b5538bda955b9c3
qqy <qyzh@users.noreply.github.com>
2024-05-16 03:58:37 +0700

    Update app\page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { BlogPosts } from 'app/components/posts'
+
 import { SpotlightPreview } from 'app/components/dotheader'
 export default function Page() {
   return (
     <section>
       <SpotlightPreview />
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
