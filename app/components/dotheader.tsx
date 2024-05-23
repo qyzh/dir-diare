2efd270ba91c02d5a656d644194881bc8b526420
qqy <qyzh@users.noreply.github.com>
2024-05-23 13:31:20 +0700

    update dotheader.tsx and layout.tsx

--- app/components/dotheader.tsx
+++ app/components/dotheader.tsx
@@ -4,7 +4,7 @@ import { Spotlight } from "app/components/spotlight";
 
 export function SpotlightPreview() {
   return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center  antialiased bg-grid-white/[0.02] relative">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
