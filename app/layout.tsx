ef65edf4b34f33dba5e2425ab6a466b1051d0cc4
qqy <qyzh@users.noreply.github.com>
2024-05-24 06:38:30 +0700

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,12 +47,12 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        'antialiased dark',
         GeistSans.variable,
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto bg-zinc-50 dark:bg-zinc-950">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
