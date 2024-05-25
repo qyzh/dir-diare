c89c5dbab49c09c71d903722b607decdb5414476
qqy <qyzh@users.noreply.github.com>
2024-05-25 23:56:10 +0700

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
   return (
   <section>
 <NavIndex/>
-<div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased sm:h-[20rem]">
+<div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased sm:h-[24rem]">
       <div className="max-w-2xl mx-auto p-4">
       <div className="text-3xl md:text-7xl font-bold mb-6 relative dark:text-zinc-100 text-zinc-700 max-w-4xl">
         Hi,
