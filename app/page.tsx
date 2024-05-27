2b9645e20512ecd512460f98e77365c3665717a7
qqy <qyzh@users.noreply.github.com>
2024-05-27 23:28:47 +0700

    style(page.tsx): :art: This will add icon
    Add icon arrow on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,45 @@ import { HoverEffect } from './components/ui/card-hover-effect'
 import { NavIndex, Navbar } from './components/nav'
 import { BackgroundBeams } from './components/ui/background-beams'
 
+function ArrowIcon() {
+    return (
+      <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke-width="1.5" 
+      stroke="currentColor" 
+      fill="none"
+      className='flex 
+        justify-center 
+        items-center 
+        select-none 
+        bg-black 
+        border-2 
+        text-neutral-300 
+        text-sm
+        font-bold 
+        p-2 
+        m-2 
+        rounded-full 
+        shadow-[0_8px_16px_rgb(0_0_0/0.4)] 
+        border border-transparent
+        border-white/[0.2]
+        h-12 
+        w-12 
+        focus:outline-none 
+        focus:shadow-outline'
+      >
+      <path 
+      stroke-linecap="round" 
+      stroke-linejoin="round" 
+      d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
+    </svg>
+    
+    );
+  }
+
 export default function Page() {
     const words = ['darling...', 'cute...', 'beautiful...', 'baby...']
     const projects = [
@@ -29,6 +68,9 @@ export default function Page() {
                         <div className="relative font-regular text-zinc-500 tracking-wide mb-8 max-w-2xl antialiased md:text-5xl">
                         Welcome to dir-diare
                     </div>
+                <div className='flex justify-center animate-bounce text-sm'><ArrowIcon/></div>
+                    
+
                     </div>
                 </div>
                 <BackgroundBeams />
