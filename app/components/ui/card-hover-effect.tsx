0c2a1c8f9992f902e4bc225781ca4eaa0f241bed
qqy <qyzh@users.noreply.github.com>
2024-05-27 23:00:58 +0700

    style: :art: This commit will change some style
    Change color background card index and add some shadow

--- app/components/ui/card-hover-effect.tsx
+++ app/components/ui/card-hover-effect.tsx
@@ -27,7 +27,7 @@ export const HoverEffect = ({
                 <Link
                     href={item?.link}
                     key={item?.link}
-                    className="relative group  block p-2 h-full w-full"
+                    className="relative group block p-2 h-full w-full"
                     onMouseEnter={() => setHoveredIndex(idx)}
                     onMouseLeave={() => setHoveredIndex(null)}
                 >
@@ -68,7 +68,7 @@ export const Card = ({
     return (
         <div
             className={cn(
-                'rounded-2xl h-full w-full p-4 overflow-hidden  border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20',
+                'rounded-2xl h-full w-full p-4 overflow-hidden bg-black shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20',
                 className
             )}
         >
