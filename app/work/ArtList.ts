cbbf68d59bd414a6d5438a8bf6db154a3bf8808a
qqy <qyzh@users.noreply.github.com>
2024-05-30 20:38:03 +0700

    update 2 files and delete 1 file

--- app/work/ArtList.ts
+++ app/work/ArtList.ts
@@ -8,33 +8,42 @@ interface ArtList {
   
   const projectsData: ArtList[] = [
     {
-      title: 'A Search Engine',
-      tagz: 'Template Blogger',
-      description: `What if you could look up any information in the world? Webpages, images, videos
-      and more. Google has many features to help you find exactly what you're looking
-      for.`,
-      imgSrc: 'https://images.unsplash.com/photo-1572010696997-c73e95aeebe8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA==&auto',
-      href: 'https://www.google.com',
-    },
-    {
-      title: 'The Time Machine',
+      title: 'Hoppla Template',
       tagz: 'Template Blogger',
       description: `Imagine being able to travel back in time or to the future. Simple turn the knob
       to the desired date and press "Go". No more worrying about lost keys or
       forgotten headphones with this simple yet affordable solution.`,
       imgSrc: 'https://images.unsplash.com/photo-1572010696997-c73e95aeebe8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA==&auto',
-      href: '/blog/the-time-machine',
+      href: 'work/hoppla',
     },
-    
     {
-        title: 'The Time Machine',
+      title: 'Rock n Troll Template',
+      tagz: 'Template Blogger',
+      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+      to the desired date and press "Go". No more worrying about lost keys or
+      forgotten headphones with this simple yet affordable solution.`,
+      imgSrc: 'https://images.unsplash.com/photo-1572010696997-c73e95aeebe8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA==&auto',
+      href: 'work/rockntroll',
+    },    
+    {
+        title: 'MinSim Template',
         tagz: 'Template Blogger',
         description: `Imagine being able to travel back in time or to the future. Simple turn the knob
         to the desired date and press "Go". No more worrying about lost keys or
         forgotten headphones with this simple yet affordable solution.`,
         imgSrc: 'https://images.unsplash.com/photo-1572010696997-c73e95aeebe8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA==&auto',
-        href: '/blog/the-time-machine',
+        href: 'work/minsim',
       },
+    {
+      title: 'Kopizine',
+      tagz: 'HTML & CSS',
+      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+      to the desired date and press "Go". No more worrying about lost keys or
+      forgotten headphones with this simple yet affordable solution.`,
+      imgSrc: 'https://images.unsplash.com/photo-1572010696997-c73e95aeebe8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA==&auto',
+      href: 'https://codepen.io/yourbaemyb/pen/bmPBxr',
+    
+    },
   ]
   
   export default projectsData
---
+++ app/work/[slug]/page.tsx
@@ -0,0 +1,108 @@
+import { notFound } from 'next/navigation'
+import { CustomMDX } from 'app/components/mdx'
+import { formatDate, getArtPosts } from 'app/work/utils'
+import { baseUrl } from 'app/sitemap'
+import { Navbar } from 'app/components/nav'
+
+export async function generateStaticParams() {
+    let posts = getArtPosts()
+
+    return posts.map((post) => ({
+        slug: post.slug,
+    }))
+}
+
+export function generateMetadata({ params }) {
+    let post = getArtPosts().find((post) => post.slug === params.slug)
+    if (!post) {
+        return
+    }
+
+    let {
+        title,
+        publishedAt: publishedTime,
+        summary: description,
+        image,
+    } = post.metadata
+    let ogImage = image
+        ? image
+        : `${baseUrl}/og?title=${encodeURIComponent(title)}`
+
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            type: 'article',
+            publishedTime,
+            url: `${baseUrl}/work/${post.slug}`,
+            images: [
+                {
+                    url: ogImage,
+                },
+            ],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description,
+            images: [ogImage],
+        },
+    }
+}
+
+export default function Blog({ params }) {
+    let post = getArtPosts().find((post) => post.slug === params.slug)
+
+    if (!post) {
+        notFound()
+    }
+
+    return (
+        <section>
+            <Navbar />
+            <script
+                type="application/ld+json"
+                suppressHydrationWarning
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        '@context': 'https://schema.org',
+                        '@type': 'BlogPosting',
+                        headline: post.metadata.title,
+                        datePublished: post.metadata.publishedAt,
+                        dateModified: post.metadata.publishedAt,
+                        description: post.metadata.summary,
+                        image: post.metadata.image
+                            ? `${baseUrl}${post.metadata.image}`
+                            : `/og?title=${encodeURIComponent(post.metadata.title)}`,
+                        url: `${baseUrl}/art/${post.slug}`,
+                        author: {
+                            '@type': 'Person',
+                            name: 'My Dir',
+                        },
+                    }),
+                }}
+            />
+            <div className="text-left mb-4">
+                <a
+                    href="/work"
+                    className="font-semibold text-neutral-300 dark:text-neutral-200"
+                >
+                    ← work
+                </a>
+            </div>
+            <p className="text-sm proportional-nums text-neutral-600 dark:text-neutral-400">
+                {formatDate(post.metadata.publishedAt)}
+            </p>
+            <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
+                {post.metadata.title}
+            </h1>
+            <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+                <CustomMDX source={post.content} />
+
+                <hr className='border-neutral-300 dark:border-neutral-700' />
+            </article>
+        </section>
+    )
+}
