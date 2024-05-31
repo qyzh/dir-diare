b31da08a4ef1cd453c7529e0736a494f51c1b759
qqy <qyzh@users.noreply.github.com>
2024-05-31 10:52:17 +0700

    style: :clown_face: This commit will change footer
    Change style link and add icon on footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,33 +1,15 @@
-function ArrowIcon() {
-    return (
-        <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-                fill="currentColor"
-            />
-        </svg>
-    )
-}
+import Link from "next/link"
 
-function AuthorIcon() {
+function Linkext({ name, href }) {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4"
+        <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-slate-50  after:content-['_↗']"
+            href={href}
         >
-            <path
-                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
-                fill="currentColor"
-            />
-        </svg>
+            {name}
+        </Link>
     )
 }
 
@@ -40,12 +22,12 @@ export default function Footer() {
                         dir-diare
                     </span>{' '}
                     by{' '}
-                    <a
+                    <Linkext
                         href="/about"
-                        className=" text-neutral-500 dark:text-neutral-200 font-semibold font-mono"
-                    >
-                        @syauqashdllh
-                    </a>{' '}
+                        name="Syauqiashadullah"
+                    />
+                    {' '}
+                    
                     make with 💔 and 💸 © {new Date().getFullYear()} All rights
                     reserved.
                 </p>
