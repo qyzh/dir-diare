4444cc28a658d226bbcf1a30a7c9f8a8bee0224b
qqy <qyzh@users.noreply.github.com>
2024-06-01 01:09:57 +0700

    update mdx.tsx, hoppla.mdx, minsim.mdx and rockntroll.mdx

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -130,6 +130,21 @@ function Marker(props) {
         </mark>
     )
 }
+
+function Error(props) {
+    return (
+<div className="px-4 text-balance py-3 border border-rose-600 bg-rose-900 rounded p-1 flex items-center text-neutral-100 mb-8">
+<div className="flex items-center w-4 mr-4">
+    
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-12">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+</svg>
+    
+</div>
+<div className="">{props.children}</div>
+</div>
+    )
+}
 function Code({ children, ...props }) {
     let codeHTML = highlight(children)
     return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
@@ -182,7 +197,7 @@ let components = {
     Kutipkiri,
     Kutipkanan,
     Linkext,
-    FN,
+    FN,Error,
     Footarea,
     FNlist,
     Marker,
