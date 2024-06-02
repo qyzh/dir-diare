bc74fad39b171c57bb7b00281866a71005bc099b
qqy <qyzh@users.noreply.github.com>
2024-06-02 17:42:06 +0700

    update 1 file and create 1 file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,11 +11,20 @@ export const metadata: Metadata = {
 export default function AboutkPage() {
     return (
         <section>
+            <div className="absolute inset-0 -z-10 h-full w-full"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]">
+                </div>
+                </div>
             <Navbar />
+
+            
             <h1 className="font-medium text-2xl mb-8 tracking-tighter">
                 about me & this blog
             </h1>
+            <div className="absolute inset-0 -z-10 bg-top opacity-10 forced-colors:hidden" 
+            style={{ 
+                backgroundImage: `url("/Image/bg-noise.png")`}} aria-hidden="true"></div>
             <div className="prose prose-neutral dark:prose-invert">
+                
                 <div className='grid grid-rows-1 md:grid-cols-2 gap-2'>
 
                     <div className='flex justify-center my-auto'>
@@ -53,6 +62,7 @@ export default function AboutkPage() {
                     </p>
 
                 </div>
+
 
             </div>
             </div>
