c8e1f52c25b209d7b3f756fa0ba288e30a735c90
qyzh <mrveranda@gmail.com>
2024-06-21 02:35:34 +0700

    update mdx.tsx

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -54,10 +54,10 @@ function RoundedImage(props) {
 }
 function Imgfull(props) {
     return (
-        <div className="flex justify-center items-center my-2 h-96 lg:h-svh md:h-svh">
+        <div className="flex mt-8 justify-center items-center my-2 h-96 lg:h-svh md:h-svh">
         <figure className="absolute left-0">
         <img className="relative h-96 lg:h-svh md:h-svh w-screen" src={props.src} alt={props.alt}/>
-        <figcaption className="relative my-2 text-sm text-center md:text-left lg:text-center text-gray-500 dark:text-gray-400">{props.caption}</figcaption>
+        <figcaption className="relative my-2 text-sm text-center md:text-center text-gray-500 dark:text-gray-400">{props.caption}</figcaption>
     </figure>
     </div>
     )
