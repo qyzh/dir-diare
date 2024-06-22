df9f2b9456272e9c0580db5aa9c2e96362c4811b
qyzh <mrveranda@gmail.com>
2024-06-22 19:53:20 +0700

    update page.tsx, pnpm-lock.yaml and tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,131 @@
 
 import React from 'react'
-import { BlogPosts } from './components/posts'
+import { Suspense } from 'react';
 import Uq from './components/uq'
+import { getBlogPosts  } from './blog/utils'
 import { Navbar } from './components/nav'
+import Link from 'next/link';
+import ArtList from './work/ArtList'
+import Image from 'next/image'
+import { CaretRightIcon, CaretUpIcon } from '@radix-ui/react-icons';
 
-
+const FBRecentWork = () => {
+    return (
+      <>
+        <div className='flex bg-zinc-800 mb-4 rounded-md'>
+        <div className='w-full min-h-[240px] overflow-hidden'>
+        <div className='absolute bottom-4 left-8 items-center py-2 px-4 rounded bg-neutral-900 animate-pulse'>Loading...</div>
+        <Image src={'/images/bg-noise.png'}  alt="Background" width={240} height={240} />
+        </div>
+        </div>
+      </>
+    );
+  };
+const FBRecentPost = () => {
+    return (
+      <>
+        <div className='flex bg-zinc-800 p-4 mb-4 rounded-md'>
+        <div className=''>
+            <div className='w-16 h-16 rounded-full bg-neutral-900 animate-pulse'></div>
+        </div>
+        <div className='flex flex-col w-full place-self-center ml-2'>
+            <div className=' bg-neutral-900 text-neutral-900 rounded animate-pulse'>Loading...</div>
+            <div className=' text-sm text-zinc-500'>Loading...</div>
+         </div>
+        </div>
+      </>
+    );
+  };
+  const RecentWork = () => {
+    return (
+      <>
+            {ArtList.slice(0,1).map((d) => (
+              <Link href={d.href || '/work'} className='group'
+                key={d.title}
+              >
+        <div className="flex bg-zinc-800 opacity-80 group-hover:opacity-100 mb-4 rounded-md">
+        <div className='w-full h-[240px] min-h-[240px] relative rounded-md overflow-clip place-self-center  overflow-hidden'>
+        <div className='absolute z-10 font-semibold block bottom-4 left-8 items-center py-2 px-4 rounded bg-black/70'>{d.title}</div>
+        <Image src={d.imgSrc || '/images/bg-noise.png'}  
+            alt="Background" className='grayscale group-hover:grayscale-0 rounded object-cover object-center group-hover:scale-125 duration-300' width={1080} height={240} />
+        </div>
+        </div>
+        </Link>
+            ))}
+      </>
+    );
+  };
+  
+  const FeaturedBlogPostsList = async () => {
+    const featuredPosts = await getBlogPosts();
+    return (
+      <>
+        {featuredPosts.slice(3)
+        .sort((a, b) => {
+            if (
+                new Date(a.metadata.publishedAt) >
+                new Date(b.metadata.publishedAt)
+            ) {
+                return -1
+            }
+            return 1
+        })
+        .map((post) => (
+            <Link
+                key={post.slug}
+                className="group"
+                href={`/blog/${post.slug}`}
+                          >
+        <div className='flex p-4 mb-4  bg-white/5 border border-transparent rounded-md duration-200 cursor-pointer hover:dark:border-zinc-700 hover:border-zinc-200'>
+        <div className='relative justify-items-center'>
+            <div className='w-16 h-16 rounded-full overflow-hidden'>
+                <Image src={post.metadata.image || '/images/bg-noise.png'}  
+                alt="Background" className='w-full h-full rounded-full object-cover overflow-hidden'  fill={true}/>
+            </div>
+        </div>
+        <div className='flex flex-col w-full place-self-center ml-2'>
+            <div className='font-semibold'>{post.metadata.title}</div>
+            <div className=' text-sm text-zinc-500'>{post.metadata.summary}</div>
+         </div>
+        </div>
+          </Link>
+        ))}
+      </>
+    );
+  };
 
 export default function Page() {
     return (
         <section>
             <Uq />
-            <BlogPosts />
-            <Navbar/>
+
+
+    
+      <Suspense fallback={<FBRecentPost />}>
+    <div className='flex justify-between mb-4'>
+      <div className='title font-semibold text-lg place-self-center '><h2>Recent Post:</h2></div>
+      <div className=''><Link href='/blog'>
+        <button className="flex text-sm items-center gap-x-2 bg-white/5 text-white-700 border border-transparent rounded-md px-4 py-2 duration-200 cursor-pointer hover:dark:border-zinc-700 hover:border-zinc-200">
+        <CaretRightIcon />
+        </button></Link>
+      </div>
+    </div>
+        <FeaturedBlogPostsList />
+      </Suspense>
+
+      <Suspense  fallback={<FBRecentWork />}>
+      <div className='flex justify-between mb-4'>
+      <div className='title font-semibold text-lg place-self-center '><h2>Recent Work:</h2></div>
+      <div className=''><Link href='/blog'>
+        <button className="flex text-sm items-center gap-x-2 bg-white/5 text-white-700 border border-transparent rounded-md px-4 py-2 duration-200 cursor-pointer hover:dark:border-zinc-700 hover:border-zinc-200">
+        <CaretRightIcon />
+        </button></Link>
+      </div>
+    </div>
+      <RecentWork/>
+      </Suspense>
+            <Navbar />
         </section>
+        
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,74 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography';
-const defaultTheme = require('tailwindcss/defaultTheme')
 
-const colors = require('tailwindcss/colors')
-const {
-    default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette')
-
-module.exports = {
-    content: [
-        '.app/**/*.{js,ts,jsx,tsx,mdx}',
-        './content/**/*.mdx',
-        '.app/components/**.{js,ts,jsx,tsx,mdx}',
-        '.app/components/ui/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            animation: {
-                'meteor-effect': 'meteor 5s linear infinite',
-                'shimmer' : "shimmer 8s infinite",
-            },
-            keyframes: {
-                meteor: {
-                    '0%': {
-                        transform: 'rotate(215deg) translateX(0)',
-                        opacity: '1',
-                    },
-                    '70%': { opacity: '1' },
-                    '100%': {
-                        transform: 'rotate(215deg) translateX(-500px)',
-                        opacity: '0',
-                    },
-                },
-                shimmer: {
-                    "0%, 90%, 100%": { 'background-position': "calc(-100% - var(--shimmer-width)) 0",
-                    },
-                    "30%, 60%": { 'background-position' : "calc(100% + var(--shimmer-width)) 0",
-                    },
-                  },
-            },
-            fontFamily: {
-                sans: ['var(--font-geist-sans)'],
-                mono: ['var(--font-geist-mono)'],
-            },
-            typography: {
-                quoteless: {
-                    css: {
-                        'blockquote p:first-of-type::before': {
-                            content: 'none',
-                        },
-                        'blockquote p:first-of-type::after': {
-                            content: 'none',
-                        },
-                    },
-                },
-            },
+export default {
+  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+      typography: {
+        quoteless: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+          },
         },
+      },
     },
-    future: {
-        hoverOnlyWhenSupported: true,
-    },
-    plugins: [typography, addVariablesForColors],
-}
-function addVariablesForColors({ addBase, theme }: any) {
-    let allColors = flattenColorPalette(theme('colors'))
-    let newVars = Object.fromEntries(
-        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-    )
-
-    addBase({
-        ':root': newVars,
-    })
-}
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+} 
