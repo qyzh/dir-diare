6d0fd8920531b79c8dddc461accbab88039e14f8
qyzh <mrveranda@gmail.com>
2024-07-03 00:50:50 +0700

    style: Remove unused icons and update date format in blog post page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,9 +5,7 @@ import { formatDate, getBlogPosts } from 'app/blog/utils'
 import { baseUrl } from 'app/sitemap'
 import { Navbar } from 'app/components/nav'
 import  Komentar  from 'app/components/comments'
-import { cn } from 'utils/cn'
 import Saweria from 'app/components/saweria'
-import { CalendarIcon, ChatBubbleIcon } from '@radix-ui/react-icons'
 import Breadcrumbs from 'app/components/breadcrumbs'
 export async function generateStaticParams() {
     let posts = getBlogPosts()
@@ -91,13 +89,19 @@ export default function Blog({ params }) {
             />
             <Breadcrumbs post={post}/>
             <div className="flex items-center gap-x-2 font-mono text-m text-neutral-600 dark:text-neutral-400 mb-2">
-<CalendarIcon />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+</svg>
+
                 <time className='proportional-nums ' dateTime={post.metadata.publishedAt} >{formatDate(post.metadata.publishedAt)}</time>
                 <Link
                 href="#comments"
                 className="flex items-center gap-x-2 font-mono text-m text-teal-400 hover:text-teal-200 "
               >
-                <ChatBubbleIcon/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+</svg>
+
                 <div className="#comments">Comments</div>
               </Link>
             </div>
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,92 +1,101 @@
 "use client"
-import { ArchiveIcon, HomeIcon, PersonIcon, RocketIcon,  } from '@radix-ui/react-icons'
-import Link from 'next/link'
-import { motion } from 'framer-motion'
-import { ProfileImage } from './ui/uqimg'
+import React from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const navItems = {
     '/': {
         name: 'home',
-        icon: <HomeIcon/>,
+        icon: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+            </svg>
+        ),
     },
     '/blog': {
         name: 'blog',
-        icon: <ArchiveIcon/>,
+        icon: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+                />
+            </svg>
+        ),
     },
     '/work': {
         name: 'work',
-        icon: <RocketIcon/>,
+        icon: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5"
+                />
+            </svg>
+        ),
     },
-}
+};
 
 export function Navbar() {
     return (
         <aside className="fixed inset-x-0 bottom-0 z-20 mx-auto mb-4 flex h-12 px-6 align-middle items-center">
-            <motion.div 
-                                initial={{
-                                    y: -40,
-                                    opacity: 0
-                                  }}
-                                  animate={{
-                                    y: 0,
-                                    opacity: 1
-                                  }}
-                                  transition={{
-                                    duration: 0.5
-                                  }}
-            
-            className="relative 
-            mx-auto flex 
-            h-full items-
-            center rounded 
-            border border-zinc-800 bg-neutral-900 dark:bg-neutral-900">
-                <nav
-                    className="flex flex-row relative"
-                    id="nav"
-                >
-                    <div className="flex flex-row align-middle items-center space-x-0">
-                        {Object.entries(navItems).map(([path, { icon,name }]) => {
+            <motion.div
+                initial={{
+                    y: -40,
+                    opacity: 0,
+                }}
+                animate={{
+                    y: 0,
+                    opacity: 1,
+                }}
+                transition={{
+                    duration: 0.5,
+                }}
+                className="relative mx-auto flex h-full items-center rounded border border-zinc-800 bg-neutral-900 dark:bg-neutral-900 p-4"
+            >
+                <nav className="flex flex-row relative" id="nav">
+                    <div className="flex flex-row align-middle items-center space-x-6">
+                        {Object.entries(navItems).map(([path, { icon, name }]) => {
                             return (
                                 <Link
                                     key={path}
                                     href={path}
-                                    className="transition-all hover:text-teal-400 rounded 
-                                    hover:bg-neutral-800 dark:hover:text-teal-400 flex 
-                                    align-middle px-4 py-4
-                                    
-                                    "
-                                    
+                                    className="transition-all hover:text-teal-400 rounded  dark:hover:text-teal-400 flex align-middle "
                                 >
-                                   {icon}
+                                    {icon}
                                 </Link>
-                            )
+                            );
                         })}
                     </div>
                 </nav>
             </motion.div>
         </aside>
-    )
-}
-
-/* header for home or index */
-
-export function NavIndex() {
-    return (
-        <aside className="-ml-[8px] tracking-tight">
-            <div className="lg:sticky lg:top-20">
-                <nav
-                    className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-                    id="nav"
-                >
-                    <div className="flex flex-row space-x-0 pl-14">
-                        <h1 className="font-mono pr-2">.dir-diare</h1>
-                        <span className="font-bold"> / </span>
-                        <span className="text-neutral-300 dark:text-neutral-500 pl-2">
-                            Random world
-                        </span>
-                    </div>
-                </nav>
-            </div>
-        </aside>
-    )
+    );
 }
