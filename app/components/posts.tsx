af7d78af43da7ce5a1038d047eb2e96803d34615
qyzh <mrveranda@gmail.com>
2024-07-03 00:02:51 +0700

    style: Refactor blog post component for improved readability and maintainability

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -26,42 +26,42 @@ export function BlogPosts() {
                         className="group"
                         href={`/blog/${post.slug}`}
                     >
-            <div className='flex 
-            flex-row items-center 
-            gap-2 
-            backdrop-blur-sm 
-            backdrop-saturate-50 
-            bg-neutral-800/20 
-            border-zinc-800 
-            px-4 py-4 border 
-            shadow-md rounded-lg mb-4 
-            group-hover:bg-neutral-800/30 
-            group-hover:border-zinc-700'
-            >
+                        <div className='flex 
+                            flex-row items-center 
+                            gap-2 
+                            backdrop-blur-sm 
+                            backdrop-saturate-50 
+                            bg-neutral-800/20 
+                            border-zinc-800 
+                            px-4 py-4 border 
+                            shadow-md rounded-lg mb-4 
+                            group-hover:bg-neutral-800/30 
+                            group-hover:border-zinc-700'
+                        >
 
-<div className=' relative h-24 w-32 rounded-md overflow-clip place-self-center '>
-<Image
-                src={post.metadata.image || '/images/bg-noise.png' }
-                className="dark:bg-zinc-800 bg-zinc-100 object-fill grayscale group-hover:grayscale-0 rounded object-cover group-hover:scale-125 duration-300"
-                alt={post.metadata.title}
-                fill={true}
-                />
-</div>
+                            <div className=' relative h-24 w-32 rounded-md overflow-clip place-self-center '>
+                                <Image
+                                    src={post.metadata.image || '/images/bg-noise.png' }
+                                    className="dark:bg-zinc-800 bg-zinc-100 object-fill grayscale group-hover:grayscale-0 rounded object-cover group-hover:scale-125 duration-300"
+                                    alt={post.metadata.title}
+                                    fill={true}
+                                />
+                            </div>
 
-<div className='w-1/2 ml-2'>
-<p className='title text-md md:text-xl font-bold'>{post.metadata.title}</p>
+                            <div className='w-1/2 ml-2'>
+                                <p className='title text-md md:text-xl font-bold'>{post.metadata.title}</p>
 
-<p className='text-sm text-zinc-500  '>
-<CalendarIcon className=' inline-block mr-1 -mt-1'/>
-<span>{formatDate(post.metadata.publishedAt, false)} </span>
+                                <p className='text-sm text-zinc-500  '>
+                                    <CalendarIcon className=' inline-block mr-1 -mt-1'/>
+                                    <span>{formatDate(post.metadata.publishedAt, true)} </span>
 
-<span>{post.metadata.tag}</span>
-</p>
-<p className='text-zinc-400 truncate overflow-hidden text-sm md:text-md'>
-{post.metadata.summary}
-</p>
-</div>
-</div>
+                                    <span>{post.metadata.tag}</span>
+                                </p>
+                                <p className='text-zinc-400 truncate overflow-hidden text-sm md:text-md'>
+                                    {post.metadata.summary}
+                                </p>
+                            </div>
+                        </div>
 
                     </Link>
                 ))}
