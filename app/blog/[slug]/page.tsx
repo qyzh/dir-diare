31ad8ab14135c696a8d1e9879f17e84c5b2b2c70
qyzh <mrveranda@gmail.com>
2024-07-19 14:52:45 +0700

    style: Update image class in posts component
    Update the image class in the posts component to remove the 'object-fill' class.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -89,7 +89,7 @@ export default function Blog({ params }) {
             />
             <Breadcrumbs post={post}/>
             <div className="flex items-center gap-x-2 font-mono text-m text-neutral-600 dark:text-neutral-400 mb-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5 stroke-indigo-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                 </svg>
 
@@ -104,7 +104,7 @@ export default function Blog({ params }) {
 
                     <div className="#comments">Comments</div>
                 </Link>
-                <TimerIcon className='inline-block '/><div className="mt-0.5">{readTime(post.content)}</div>
+                <TimerIcon className='inline-block stroke-sky-500'/><div className="mt-0.5">{readTime(post.content)}</div>
             </div>
             
             <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
