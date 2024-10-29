98dd44ce35af6f580e8ee4fc52697c82b1ea8677
syauqi ashadullah <qyzh@users.noreply.github.com>
2024-10-29 12:18:55 +0700

    refactor: :construction: Update Saweria Link
    Update saweria Link

--- app/components/saweria.tsx
+++ app/components/saweria.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Saweria() {
     return (
       <a
-        href="https://saweria.co/qyzh"
+        href="https://saweria.co/UQ14"
         className="flex items-center flex-row justify-around text-start gap-2 min-h-[110px]  hover:dark:bg-[#2e290e44] rounded-lg border dark:border-zinc-800 border-zinc-200 hover:dark:border-[#ffdd0060] hover:border-[#e6d14fe7] duration-300 p-6 group"
         target="_blank"
         rel="noreferrer noopener"
