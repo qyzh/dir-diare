116601a3ce20ae6c1c62a27ea29805275fa4cbf5
syauqi ashadullah <qyzh@users.noreply.github.com>
2025-05-11 21:16:18 +0700

    feat: add PhotoGallery component and integrate it into the gallery page

---
+++ app/galery/page.tsx
@@ -0,0 +1,32 @@
+
+import {  Navbar } from '../components/nav'
+import { SecHeadBlog } from 'app/components/sechead';
+import Breadcrumbs from 'app/components/breadcrumbs';
+import { AnimatedAbove, AnimatedBelow } from 'app/components/animated-section';
+import Footer from 'app/components/footer';
+import PhotoGallery from 'app/components/photo';
+
+export const metadata = {
+    title: 'Galery',
+    description: 'My Galery.',
+}
+
+export default function Page() {
+    return (
+        <section>
+            <Navbar />
+            <AnimatedAbove delay={0.5}>
+            <Breadcrumbs />
+            </AnimatedAbove>
+            <AnimatedAbove delay={0.3}>
+            <SecHeadBlog />
+            </AnimatedAbove>
+           <AnimatedBelow delay={0.3}>
+             <PhotoGallery username="syauqashdllh" limit={12} />
+            </AnimatedBelow>
+            <AnimatedBelow delay={1.0}>
+            <Footer/>
+            </AnimatedBelow>
+        </section>
+    )
+}
