844d09c5a2d765ad5c7c316d17e7e8f31eebae3a
syauqi ashadullah <qyzh@users.noreply.github.com>
2025-05-20 05:03:36 +0700

    fix: update tag display to use category in RecentWork component

--- app/components/work-list.tsx
+++ app/components/work-list.tsx
@@ -47,7 +47,7 @@ const SRW = () => {
         className="absolute inset-0 h-full w-full grayscale group-hover:grayscale-0 overflow-hidden object-cover object-center opacity-75 rounded transition-opacity group-hover:opacity-50" alt={''}  />
   <div className="relative p-4 sm:p-6 lg:p-8">
     <p className="text-sm font-medium uppercase tracking-widest text-teal-400">
-    {d.metadata.tag}
+    {d.metadata.category}
     </p>
     <p className="text-xl font-bold text-white sm:text-2xl">
     {d.metadata.title}
