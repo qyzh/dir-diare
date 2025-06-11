9e4c6e31470986e4fd5fa44814f043f141c865c3
syauqi ashadullah <qyzh@users.noreply.github.com>
2025-06-11 08:19:02 +0700

    feat(component): add new component `UKpathdir`

---
+++ app/components/ukpathdir.tsx
@@ -0,0 +1,30 @@
+"use client"
+import React from 'react';
+
+interface UKpathdirProps {
+    name:string,
+    type:string
+}
+
+const UKpathdir: React.FC<UKpathdirProps> = ({
+    name,
+    type
+}) => {
+    return (
+
+        <p className='text-sm text-neutral-500 font-mono'>
+                <span className='text-neutral-400 dark:text-neutral-300 mr-0.5'>dir-diare</span>
+                /
+                {name && (
+                <span className='text-neutral-600 dark:text-neutral-300 ml-0.5'><a href=''>{name}</a></span>
+                )}
+                .
+                {type && (
+                <span>{type}</span>
+                )}
+        </p>
+
+    );
+};
+
+export default UKpathdir;
