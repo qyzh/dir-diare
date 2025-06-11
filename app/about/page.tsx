7f8c90b9ff517727020dee99f6b40de3847e32f9
syauqi ashadullah <qyzh@users.noreply.github.com>
2025-06-11 09:38:58 +0700

    feat(about): apply `ukpathdir` and adjust `animation`

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { Navbar } from 'app/components/nav';
-import { AnimatedAbove, AnimatedZoom, AnimatedRight, AnimatedFade } from 'app/components/animated-section';
+import { AnimatedAbove, AnimatedZoom, AnimatedRight, AnimatedBelow } from 'app/components/animated-section';
 import Breadcrumbs from 'app/components/breadcrumbs';
 import Uq from 'app/components/uq';
 import Footer from 'app/components/footer';
+import UKpathdir from 'app/components/ukpathdir';
 
 export const metadata: Metadata = {
     title: 'About',
@@ -20,14 +21,8 @@ interface Edu {
 
 const About: React.FC = () => (
     <AnimatedAbove delay={0.5}>
-        <div className="flex flex-col gap-2 p-4 my-4 border-1 border-neutral-300 dark:border-neutral-700 rounded">
+        <div className="flex flex-col gap-2 p-4 border-1 border-neutral-300 dark:border-neutral-700 rounded">
             <AnimatedZoom delay={0.8}>
-                <p className='text-sm text-neutral-500 font-mono'>
-                    <span className='text-neutral-400 dark:text-neutral-300 mr-0.5'>dir-diare</span>
-                    /
-                    <span className='text-neutral-600 dark:text-neutral-300 ml-0.5'>about</span>
-                    .md
-                </p>
                 <p className="prose prose-neutral dark:prose-invert">
                     I am a <span className="italic border-b-2 border-emerald-950 bg-emerald-500/50 hover:bg-emerald-500">Communication student</span> who has an interest in <span className="italic border-b-2 border-emerald-950 bg-emerald-500/50 hover:bg-emerald-500">interface design</span> & <span className="italic border-b-2 border-emerald-950 bg-emerald-500/50 hover:bg-emerald-500">running</span>. My name is{' '}
                     <span className="italic border-b-2 border-emerald-950 bg-emerald-500/50 hover:bg-emerald-500">Syauqi Ashadullah</span> and am currently living in Bandung, West Java.
@@ -71,13 +66,7 @@ const EduTimelineItem: React.FC<Edu> = ({ title, desc, year, place }) => (
 );
 
 const EduTL: React.FC = () => (
-    <AnimatedZoom delay={0.5}>
-        <p className='text-sm text-neutral-500 font-mono mb-2'>
-            <span className='text-neutral-400 dark:text-neutral-300 mr-0.5'>dir-diare</span>
-            /
-            <span className='text-neutral-600 dark:text-neutral-300 ml-0.5'>edu</span>
-            .md
-        </p>
+    <AnimatedZoom delay={0.5} className="grid grid-cols-1 gap-2">
         <div className="relative space-y-6">
             <div className="relative px-4 space-y-8 before:absolute before:top-2 before:bottom-0 before:w-0.5 before:bg-neutral-400 before:dark:bg-neutral-900">
                 {eduData.map((edu, index) => (
@@ -91,14 +80,7 @@ const EduTL: React.FC = () => (
 );
 
 const DearDairy: React.FC = () => (
-    <div className="flex flex-col gap-2 p-4 my-4 border-1 border-neutral-300 dark:border-neutral-700 rounded">
-        <AnimatedZoom delay={0.8}>
-            <p className='text-sm text-neutral-500 font-mono'>
-                <span className='text-neutral-400 dark:text-neutral-300 mr-0.5'>dir-diare</span>
-                /
-                <span className='text-neutral-600 dark:text-neutral-300 ml-0.5'>deardairy</span>
-                .md
-            </p>
+        <AnimatedZoom delay={0.8} className="flex flex-col gap-2 p-4 border-1 border-neutral-300 dark:border-neutral-700 rounded">
             <div className="prose prose-neutral dark:prose-invert">
                 <strong>Why the name dir-diare?</strong>
                 <p>It started with the 
@@ -113,7 +95,7 @@ const DearDairy: React.FC = () => (
                 <p>So came <strong>dir-diare</strong>:</p>
                 <ul className="list-disc pl-6">
                     <li>
-                    <strong>"dir" </strong>is short for {'  '}<i>directory</i>{'  '} in programming—where we dump files. In this case, it's where I dump thoughts, rants, ideas, and brain-farts.
+                    <strong className='font-mono'>"dir" </strong>is short for {'  '}<code className='font-mono'>directory</code>{'  '} in programming—where we dump files. In this case, it's where I dump thoughts, rants, ideas, and brain-farts.
                     </li>
                     <li>
                     <strong>"diare" </strong>(a cheeky twist on "diary") literally means {'  '}<i>diarrhoea</i>{'  '} in Indonesian. Which, fun fact: I actually get if I drink milk. But also, it perfectly captures how my thoughts sometimes rush out in... uncontrollable bursts. 💻💩
@@ -124,48 +106,39 @@ const DearDairy: React.FC = () => (
                 </p>
             </div>
         </AnimatedZoom>
-    </div>
 );
 
 const UCanFindMe: React.FC = () => (
-<div className="flex flex-col font-mono gap-2 p-4 my-4 border-1 border-neutral-300 dark:border-neutral-700 rounded">
-            <p className='text-sm text-neutral-500 font-mono'>
-                <span className='text-neutral-400 dark:text-neutral-300 mr-0.5'>dir-diare</span>
-                /
-                <span className='text-neutral-600 dark:text-neutral-300 ml-0.5'>u-can-find-me</span>
-                .md
-            </p>
-            <span>
+<AnimatedBelow delay={1.0} className="flex flex-col font-mono">
+    <div className='pl-4'>
+            <p>
             You can find me:
-            </span>
-            <ul>
+            </p>
+            <ul className='pl-4'>
                 <li>Github:<a href='https://github.com/qyzh' target='_blank'><span className='text-sm font-mono py-0.5 px-1.5 text-neutral-400 hover:bg-neutral-900 hover:border-neutral-800 hover:border '>qyzh</span></a> </li>
                 <li>Discord:<a href='https://discord.com/users/334529486773026817' target='_blank'><span className='text-sm font-mono py-0.5 px-1.5 text-neutral-400 hover:bg-neutral-900 hover:border-neutral-800 hover:border '>UQ#9662</span></a></li>
                 <li>Twitter:<a href='https://x.com/asetdunia' target='_blank'><span className='text-sm font-mono py-0.5 px-1.5 text-neutral-400 hover:bg-neutral-900 hover:border-neutral-800 hover:border '>asetdunia</span></a> </li>
                 <li>LinkedIn:<a href='/' target='_blank'><span className='text-sm font-mono py-0.5 px-1.5 text-neutral-400 hover:bg-neutral-900 hover:border-neutral-800 hover:border '>qyzh</span></a> </li>
                 <li>Instagram:<a href='https://www.instagram.com/syauqashdllh/' target='_blank'><span className='text-sm font-mono py-0.5 px-1.5 text-neutral-400 hover:bg-neutral-900 hover:border-neutral-800 hover:border '>syauqashdllh</span></a> </li>
             </ul>
-</div>
+    </div>
+</AnimatedBelow>
 );
 
 const AboutPage: React.FC = () => (
     <section>
-        <AnimatedFade delay={0.2}>
-            <Breadcrumbs />
-        </AnimatedFade>
-        <AnimatedAbove delay={0.2}>
+        <Breadcrumbs />
+        <main>
             <Uq />
-        </AnimatedAbove>
-        <About />
-        <AnimatedFade delay={0.5}>
+            <UKpathdir name='about-me' type='md'/>
+            <About />
+            <UKpathdir name='dear-diary' type='md'/>
             <DearDairy />
-        </AnimatedFade>
-        <div className="grid grid-cols-1 gap-2">
+            <UKpathdir name='edu' type='md'/>
             <EduTL />
-        </div>
-        <AnimatedFade delay={0.5}>
+            <UKpathdir name='contact' type='md'/>
             <UCanFindMe />
-        </AnimatedFade>
+        </main>
         <Navbar />
         <Footer />
     </section>
