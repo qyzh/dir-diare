5f1cd4025de09f916eb245a07c7793ace455ef14
syauqi ashadullah <qyzh@users.noreply.github.com>
2025-06-18 01:53:20 +0700

    refactor(ukrunning): component to accept race data as props and improve rendering logic

--- app/components/ukrunning.tsx
+++ app/components/ukrunning.tsx
@@ -1,86 +1,62 @@
-'use client';
-
-import { CalendarHeart, Dot, GripVertical, Map } from 'lucide-react';
-import React from 'react';
+import { CalendarHeart, Dot, Map } from 'lucide-react';
 
 interface UKRunningProps {
-  date?: string;
-  place?: string;
-  title?: string;
-  description?: string;
-  distance?: number;
-  time?: string;
-  pace?: number;
-  children?: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
+  ukrace: any[] | null;
 }
 
-const UKRunning: React.FC<UKRunningProps> = ({
-  date,
-  place,
-  title,
-  description,
-  distance,
-  time,
-  pace,
-  children,
-  className = '',
-  onClick,
-}) => {
-    const handleClick = () => {
-        if (onClick) {
-        onClick();
-        }
-  };
-
+export default function UKRunning({ ukrace }: UKRunningProps){
   return (
-    <div 
-      className={`${className} bg-white/5 border border-transparent transition-all duration-200 my-4`}
-      onClick={onClick}
-    >
-        <div className="flex flex-col gap-2 p-4 justify-between">
+    <div className="mb-2">
+      {ukrace?.sort((a, b) => new Date(b.date || '').getTime() - new Date(a.date || '').getTime()).map((item, index) => (
+        <div 
+          key={index}
+          className="bg-white/5 border border-transparent transition-all duration-200 my-4"
+        >
+            <div className="flex flex-col gap-2 p-4 justify-between">
+                <div className="flex flex-col gap-2">
+                  <div className="flex">
+                    {item.date && 
+                      <span className="text-xs flex items-center text-white/50">
+                          <CalendarHeart className="inline w-4 h-4 mr-1" />
+                          <span className="hover:text-white/80 transition-colors">{item.date}</span>
+                      </span>
+                    }
+                    <Dot className="inline w-4 h-4 text-white/30 mx-0.5" />
+                    {item.place && 
+                      <span className="text-xs flex items-center text-white/50">
+                          <Map className="inline w-4 h-4 mr-1" />
+                          <span className="hover:text-white/80 transition-colors">{item.place}</span>
+                      </span>
+                    }
+                  </div>
+                  {item.title && <h2 className="text-2xl font-semibold truncate text-white">{item.title}</h2>}
+                  {item.description && <p className="text-white/70">{item.description}</p>}
+                </div>
 
-            <div className="flex flex-col gap-2">
-            <div className="flex">
-              {date && 
-                <span className="text-xs flex items-center text-white/50">
-                    <CalendarHeart className="inline w-4 h-4 mr-1" /><span className="hover:text-white/80 transition-colors">{date}</span></span>
-              }
-              <Dot className="inline w-4 h-4 text-white/30 mx-0.5" />
-              {place && 
-                <span className="text-xs flex items-center text-white/50">
-                    <Map className="inline w-4 h-4 mr-1" /><span className="hover:text-white/80 transition-colors">{place}</span></span>
-              }
-            </div>
-            {title && <h2 className="text-2xl font-semibold truncate text-white">{title}</h2>}
-            {description && <p >{description}</p>}
-            </div>
-
-            <div className="grid grid-cols-3 gap-2">
-            {distance && 
-            <div className="text-sm flex flex-col items-center justify-center text-white/50 bg-black/50 p-4 rounded">
-                <div className="hover:text-white/80 transition-colors font-mono tabular-nums">{distance}km</div> 
-                <div className="text-white/50">Distance</div>
-            </div>
-            }
-            {time && 
-            <div className="text-sm flex flex-col items-center justify-center text-white/50 bg-black/50 p-1 rounded">
-                <div className="hover:text-white/80 transition-colors font-mono tabular-nums">{time}</div> 
-                <div className="text-white/50">Time</div>
-            </div>
-            }
-            {pace && 
-            <div className="text-sm flex flex-col items-center justify-center text-white/50 bg-black/50 p-1 rounded">
-                <div className="hover:text-white/80 transition-colors font-mono tabular-nums">{pace}min/km</div>
-                <div className="text-white/50">Pace</div>
-            </div>
-            }
-            {children}
+                <div className="grid grid-cols-3 gap-2">
+                  {item.distance && 
+                    <div className="text-sm flex flex-col items-center justify-center text-white/50 bg-black/50 p-4 rounded">
+                        <div className="hover:text-white/80 transition-colors font-mono tabular-nums">{item.distance}km</div> 
+                        <div className="text-white/50">Distance</div>
+                    </div>
+                  }
+                  {item.time && 
+                    <div className="text-sm flex flex-col items-center justify-center text-white/50 bg-black/50 p-1 rounded">
+                        <div className="hover:text-white/80 transition-colors font-mono tabular-nums">{item.time}</div> 
+                        <div className="text-white/50">Time</div>
+                    </div>
+                  }
+                  {item.pace && 
+                    <div className="text-sm flex flex-col items-center justify-center text-white/50 bg-black/50 p-1 rounded">
+                        <div className="hover:text-white/80 transition-colors font-mono tabular-nums">{item.pace}min/km</div> 
+                        <div className="text-white/50">Pace</div>
+                    </div>
+                  }
+                </div>
             </div>
         </div>
+      ))}
     </div>
   );
-};
+}
 
-export default UKRunning;
--- app/r/page.tsx
+++ app/r/page.tsx
@@ -3,6 +3,7 @@ import Footer from 'app/components/footer';
 import { Navbar } from 'app/components/nav';
 import UKDesc from 'app/components/ukDesc';
 import UKRunning from 'app/components/ukrunning';
+import { createClient } from '../api/supabase/server';
 
 const title = 'running';
 const description = 'Running is my way to stay fit and healthy. I run for fun and to challenge myself. Running Is Our Therapy';
@@ -11,41 +12,16 @@ export const metadata = {
     description: `${description}`,
 }
 
-export default async function Notes() {
+export default async function Race() {
+  const supabase = await createClient();
+  const { data: ukrace } = await supabase.from("ukrace").select();
+  
   return (
   <section>
     <Breadcrumbs/>
     <Navbar/>
     <UKDesc title='r' description={description} />
-
-    <UKRunning 
-    date="2024.08.11"
-    place="Kota Bandung, ID"
-    title="Qris run 2024" 
-    className="hover:border-blue-600" 
-    distance={7.27} time="56:48" pace={8.05} />
-
-    <UKRunning 
-    date="2024.07.21"
-    place="Kota Bandung, ID"
-    title="Pocari Sweat Run 2024" 
-    className="hover:border-sky-600" 
-    distance={21.28} time="3:07:46" pace={8.49} />
-
-    <UKRunning 
-    date="2024.03.20"
-    place="Kota Bandung, ID"
-    title="Duraking fun run 2024" 
-    className="hover:border-white" 
-    distance={10.15} time="1:33:46" pace={9.14} />
-
-    <UKRunning 
-    date="2023.11.19"
-    place="Magelang, ID"
-    title="Borobudur Marathon 2023" 
-    className="hover:border-orange-600" 
-    distance={10.30} time="1:27:09" pace={8.47} />
-
+    <UKRunning ukrace={ukrace} />
     <Footer/>
   </section>
   );
