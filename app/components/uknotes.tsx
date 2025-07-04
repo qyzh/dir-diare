40300c263b0bb56fb801c0818b0ff48d4cfaacd8
syauqi ashadullah <qyzh@users.noreply.github.com>
2025-07-04 13:42:13 +0700

    refactor(uknotes): update data source and modify note display properties

--- app/components/uknotes.tsx
+++ app/components/uknotes.tsx
@@ -3,23 +3,23 @@ import { createClient } from '../api/supabase/server';
 
 export default async function UKnotes() {
   const supabase = await createClient();
-  const { data: uknotes } = await supabase.from("uknotes").select();
+  const { data: notes } = await supabase.from("notes").select();
 
   return (
     <div className="mb-2">
-        {uknotes?.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()).map((item) => (
+        {notes?.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()).map((item) => (
           <blockquote key={item.id} className="group mb-4 border-b border-transparent hover:border-amber-200">
             <p className="text-2xl tracking-wider mb-2 opacity-80 group-hover:opacity-100 ">
-              "{item.notes}"
+              "{item.quote}"
             </p>
             <div className='flex justify-between'>
             <p className="text-sm text-neutral-600 dark:text-neutral-400 opacity-80 group-hover:opacity-100 ">
-             - <Link href={item.link} className='hover:underline hover:text-neutral' target='_blank'>
-              {item.source} 
+             - <Link href={item.source} className='hover:underline hover:text-neutral' target='_blank'>
+              {item.author} 
               </Link>
             </p>
               <time className="text-xs text-neutral-500 dark:text-neutral-400 opacity-80 group-hover:opacity-100">
-              ({new Date(item.timestamp).toLocaleDateString('en-US', { 
+              ({new Date(item.timestamptz).toLocaleDateString('id-ID', { 
                 year: 'numeric',
                 month: 'short',
                 day: 'numeric',
