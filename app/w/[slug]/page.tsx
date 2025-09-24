55146f566579f4508cfebe3a713f825dc8aa3595
qyzh <mrveranda@gmail.com>
2025-09-24 22:46:50 +0700

    Style : update UI for slug/post

--- app/w/[slug]/page.tsx
+++ app/w/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import { readdir } from 'fs/promises'
 import fs from 'fs'
 import path from 'path'
-
-import { Navbar } from 'app/components/nav'
 import Breadcrumbs from 'app/components/breadcrumbs'
 import Comments from 'app/components/comments'
 import { formatDate } from '../utils'
@@ -11,74 +9,63 @@ import Link from 'next/link'
 
 export default async function Page({
     params,
-  }: {
+}: {
     params: Promise<{ slug: string }>
-  }) {
+}) {
     const { slug } = await params
     const filePath = path.join(process.cwd(), 'content', `${slug}.mdx`)
     const fileContent = fs.readFileSync(filePath, 'utf-8')
-    
+
     // Parse frontmatter
     const frontmatterRegex = /---\s*([\s\S]*?)\s*---/
     const match = frontmatterRegex.exec(fileContent)
     const frontMatterBlock = match?.[1] || ''
     const metadata: Record<string, string> = {}
-    
+
     frontMatterBlock.split('\n').forEach((line) => {
-      const [key, ...valueArr] = line.split(': ')
-      if (key && valueArr.length) {
-        const value = valueArr.join(': ').trim()
-        metadata[key.trim()] = value.replace(/^['"](.*)['"]$/, '$1')
-      }
+        const [key, ...valueArr] = line.split(': ')
+        if (key && valueArr.length) {
+            const value = valueArr.join(': ').trim()
+            metadata[key.trim()] = value.replace(/^['"](.*)['"]$/, '$1')
+        }
     })
 
     const { default: Post } = await import(`../../../content/${slug}.mdx`)
-   
+
     return (
-      <section>
-      <Breadcrumbs/>
-      <Navbar />
-      <h1 className="text-3xl font-bold ">{metadata.title || slug.replace(/-/g, ' ')}</h1>
-      <div className="flex items-center space-x-2 my-2">
-      <div className="text-sm font-mono text-neutral-500 dark:text-neutral-400">
-          <div className="flex items-center gap-2">
-            <img 
-              src="/images/profil.jpg" 
-              alt="Author avatar" 
-              className="w-5 h-5 rounded-full bg-teal-300"
-            />
-            <Link
-            href='/about'
-            className='text-black dark:text-white hover:text-emerald-300 transition-colors duration-200'
-            title="About the author"
-            >qyzh</Link>
-          </div>
-      </div>
-      <span className="text-sm font-mono text-neutral-500 dark:text-neutral-400">•</span>
-      <time className="text-sm font-mono text-neutral-500 dark:text-neutral-400">
-        {metadata.publishedAt ? formatDate(metadata.publishedAt) : 'Unknown date'}
-      </time>
-      </div>
-      <p className="text-black/50 dark:text-neutral-500 font-mono my-4">
-        {metadata.summary || ''}
-      </p>      
-      <div className="max-w-none">
-        <Post />
-      </div>
-      <Comments />
-      <Footer/>
-      </section>
+        <section>
+            <Breadcrumbs />
+            <h1 className="text-xl font-bold ">
+                {metadata.title || slug.replace(/-/g, ' ')}
+            </h1>
+            <div className="flex items-center space-x-2 my-2">
+                <time className="text-sm font-mono text-neutral-500 dark:text-neutral-400">
+                    {metadata.publishedAt
+                        ? formatDate(metadata.publishedAt)
+                        : 'Unknown date'}
+                </time>
+            </div>
+            <p className="text-black/50 dark:text-neutral-500 font-mono my-4">
+                {metadata.summary || ''}
+            </p>
+            <div className="max-w-none">
+                <Post />
+            </div>
+            <Comments />
+            <Footer />
+        </section>
     )
-  }
-   
-export async function generateStaticParams() {
-  const contentDir = path.join(process.cwd(), 'content')
-  const files = await readdir(contentDir)
-  const mdxFiles = files.filter(file => file.endsWith('.mdx'))
-  
-  return mdxFiles.map(file => ({
-    slug: file.replace('.mdx', '')
-  }))
 }
-   
+
+export async function generateStaticParams() {
+    const contentDir = path.join(process.cwd(), 'content')
+    const files = await readdir(contentDir)
+    const mdxFiles = files.filter((file) => file.endsWith('.mdx'))
+
+    return mdxFiles.map((file) => ({
+        slug: file.replace('.mdx', ''),
+    }))
+}
+
 export const dynamicParams = false
+
