9468a8700c8bdd6a06ebbdff90a272263b35a9db
uki <qyzh@users.noreply.github.com>
2025-09-24 22:50:16 +0700

    Revert "Update UI"

--- app/components/discord.tsx
+++ app/components/discord.tsx
@@ -1,67 +1,213 @@
-'use client'
+"use client";
 
-import { useLanyard } from 'react-use-lanyard'
-import type { Activity } from 'react-use-lanyard'
-import { useEffect, useState } from 'react'
+import { useLanyard } from "react-use-lanyard";
+import type { Activity } from "react-use-lanyard";
+import { useEffect, useState } from "react";
+import avatarGithubUrl from "public/images/profil.jpg";
+interface DiscordStatusProps {
+    userId?: string;
+    className?: string;
+}
+const Badge_discord_Data = [
+    {
+      "id": "premium",
+      "description": "Subscriber since Apr 11, 2025",
+      "icon": "2ba85e8026a8614b640c2837bcdfe21b",
+      "link": "https://discord.com/settings/premium"
+    },
+    {
+      "id": "hypesquad_house_2",
+      "description": "HypeSquad Brilliance",
+      "icon": "011940fd013da3f7fb926e4a1cd2e618",
+      "link": "https://discord.com/settings/hypesquad-online"
+    },
+    {
+      "id": "active_developer",
+      "description": "Active Developer",
+      "icon": "6bdc42827a38498929a4920da12695d9",
+      "link": "https://support-dev.discord.com/hc/en-us/articles/10113997751447?ref=badge"
+    },
+    {
+      "id": "early_supporter",
+      "description": "Early Supporter",
+      "icon": "7060786766c9c840eb3019e725d2b358",
+      "link": "https://discord.com/settings/premium"
+    },
+    {
+      "id": "guild_booster_lvl1",
+      "description": "Server boosting since Apr 16, 2025",
+      "icon": "51040c70d4f20a921ad6674ff86fc95c",
+      "link": "https://discord.com/settings/premium"
+    },
+    {
+      "id": "legacy_username",
+      "description": "Originally known as UQ#9662",
+      "icon": "6de6d34650760ba5551a79732e98ed60"
+    },
+    {
+      "id": "quest_completed",
+      "description": "Completed a Quest",
+      "icon": "7d9ae358c8c5e118768335dbe68b4fb8",
+      "link": "https://discord.com/discovery/quests"
+    }
+]
+const BadgeDiscord = () => {
 
-export function DiscordStatus() {
-    const [activity, setActivity] = useState<Activity | undefined>(undefined)
-    const userId = process.env.NEXT_PUBLIC_DISCORD_USER_ID || ''
+    return (
+        <>
+
+{Badge_discord_Data.map((dc) => (
+            <img
+                key={dc.id}
+                src={`https://cdn.discordapp.com/badge-icons/${dc.icon}.png`}
+                alt={dc.description}
+                title={dc.description}
+                className="w-4 h-4 rounded"
+            />
+        ))}
+
+        </>
+    );
+}
+
+export function DiscordStatus({ userId = process.env.NEXT_PUBLIC_DISCORD_USER_ID || '', className = "" }: DiscordStatusProps) {
+    const [activity, setActivity] = useState<Activity | undefined>(undefined);
 
     const { loading, status } = useLanyard({
         userId,
         socket: true,
-    })
+    });
 
     useEffect(() => {
         if (status?.activities) {
-            setActivity(
-                status.activities.find((activity) => activity.type !== 4)
-            )
+            setActivity(status.activities.find((activity) => activity.type !== 4));
         }
-    }, [status])
+    }, [status]);
 
     if (loading || !status?.discord_user) {
-        return <div className="flex items-center space-x-3"></div>
+        return (
+            <div className="flex items-center space-x-3">
+                <div className="relative">
+                    <div className="w-[120px] h-[120px] rounded bg-neutral-800/50 animate-pulse" />
+                    <div className="absolute bottom-0 right-0 w-4 h-4 bg-neutral-700 rounded-full border-2 border-zinc-900 animate-pulse" />
+                </div>
+                <div className="flex flex-col overflow-hidden">
+                    <div className="flex items-center gap-2 ">
+                        <div className="h-4 w-20 bg-neutral-800/50 rounded animate-pulse" />
+                        <div className="h-4 w-12 bg-neutral-800/50 rounded-2xl animate-pulse" />
+                    </div>
+                    <div className="h-4 w-28 bg-neutral-800/50 rounded mt-1 animate-pulse" />
+                    <div className="mt-3">
+                        <div className="flex items-center">
+                            <div className="h-8 w-8 bg-neutral-800/50 mr-2 rounded animate-pulse" />
+                            <div className="flex flex-col gap-1">
+                                <div className="h-4 w-28 bg-neutral-800/50 rounded animate-pulse" />
+                                <div className="h-3 w-28 bg-neutral-800/50 rounded animate-pulse" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 
-    const statusColor =
-        {
-            online: 'text-green-500',
-            idle: 'text-yellow-500',
-            dnd: 'text-red-500',
-            offline: 'text-gray-500',
-        }[status.discord_status] || 'text-white-500'
+    const avatarUrl = status.discord_user.avatar
+        ? `https://cdn.discordapp.com/avatars/${status.discord_user.id}/${status.discord_user.avatar}`
+        : `https://cdn.discordapp.com/embed/avatars/${parseInt(status.discord_user.discriminator) % 5}.png`;
+
+    const statusColor = {
+        online: "bg-green-500",
+        idle: "bg-yellow-500",
+        dnd: "bg-red-500",
+        offline: "bg-gray-500"
+    }[status.discord_status] || "bg-gray-500";
+
+const guild = (status.discord_user as any).primary_guild;
+const { tag, identity_guild_id: Guild_Id, badge: Guild_Badge } = guild || {};
+const Guild_IconUrl = Guild_Id && Guild_Badge ? `https://cdn.discordapp.com/clan-badges/${Guild_Id}/${Guild_Badge}.png?size=16` : null;
 
     return (
-        <div className="ml-6">
-            <div>
-                <span className="webtree">└──</span>
-                <span className="websub">username/</span>
-                <span className={`${statusColor}`}>
-                    {status.discord_user.username}
-                </span>
-            </div>
-            <div>
-                <span className="webtree">└──</span>
-                <span className="websub">activity/</span>
-                <span className="webcontent">
-                    {activity && (
-                        <span>
-                            {activity.name}
-                            {activity.details && (
-                                <span> {activity.details}</span>
-                            )}
-                            {activity.state && (
-                                <span>
-                                    {activity.details ? ' @ ' : ''}
-                                    {activity.state}
-                                </span>
-                            )}
+        <div className="group bg-[url('https://dcdn.dstn.to/banners/334529486773026817')] bg-cover bg-center bg-no-repeat rounded relative overflow-hidden">
+            <div className="absolute inset-0 backdrop-blur-sm group-hover:backdrop-blur-none transition-all duration-300" />
+            <div className={`flex items-center space-x-3 ${className} relative z-10`}>
+                <div className="relative">
+                    <img
+                        src={avatarGithubUrl.src}
+                        alt={`${status.discord_user.username}'s avatar`}
+                        width={96}
+                        height={96}
+                        className="rounded w-24 h-24 object-cover"
+                    />
+                    <div className={`absolute bottom-0 right-0 w-4 h-4 ${statusColor} rounded-full border-2 border-zinc-900`} />
+                </div>
+                <div className="flex flex-col">
+                    <div className="flex items-center gap-2">
+                        <span className="text-2xl font-bold text-neutral-200">
+                            {status.discord_user.global_name || status.discord_user.username}
                         </span>
+    {Guild_IconUrl && tag && (
+        <span className="text-xs font-semibold inline-flex items-center px-1 py-0.5 text-zinc-200 bg-white/20 rounded border border-neutral-500 hover:bg-neutral-700 transition-all">
+            <img
+                src={Guild_IconUrl}
+                alt="Guild Icon"
+                width={12}
+                height={12}
+                className="inline-block mr-1"
+            />
+            {tag}
+        </span>
+    )}
+                    </div>
+                    <span className="text-sm font-mono italic text-neutral-400 group-hover:text-white">
+                        @{status.discord_user.username} • he/him
+                    </span>
+                    <div className="flex items-center gap-1 mt-1">
+                        {/* Discord badges */}
+                        <BadgeDiscord/>
+                    </div>
+                    {activity && (
+                        <div className="text-xs text-neutral-400 mt-1">
+
+                                {activity.name === "Spotify" ? (
+                                    <div className="flex items-center gap-1">
+                                        <img
+                                            src={status.spotify?.album_art_url || `https://dcdn.dstn.to/app-icons/${activity.application_id}.png`}
+                                            alt="Spotify"
+                                            className="h-8 w-8 rounded border border-transparent group-hover:border-green-500 transition-all duration-300"
+                                        />
+                                        <div className="flex flex-col ml-2 group">
+                                            <a href="/m" className="font-semibold text-green-600 group-hover:text-green-400 transition-colors">
+                                                {status.spotify?.song}
+                                            </a>
+                                            <span className="font-mono group-hover:text-neutral-100 transition-colors">
+                                                by {status.spotify?.artist?.replace(/;/g, ', ')}
+                                            </span>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    activity.assets?.large_image && (
+                                        <div className="flex items-center gap-1">
+                                            <img
+                                                src={`https://dcdn.dstn.to/app-icons/${activity.application_id}.png`}
+                                                alt="Activity"
+                                                className="h-8 w-8 rounded"
+                                            />
+                                            <div className="flex flex-col ml-2">
+                                                <span className="font-semibold text-neutral-200">
+                                                    {activity.name}
+                                                </span>
+                                                <div className="font-mono">
+                                                    {activity.details && <span>{activity.details}</span>}
+                                                    {activity.state && <span>{activity.details ? " @ " : ""}{activity.state}</span>}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                )}
+                            </div>
                     )}
-                </span>
+                </div>
             </div>
         </div>
-    )
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,30 +7,29 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import { monaspace } from './fonts'
 
 export const metadata: Metadata = {
-    metadataBase: new URL('https://dir-diare.vercel.app'),
-    alternates: {
-        canonical: '/',
-    },
-    title: {
-        default: 'Dir-diare',
-        template: '%s | Dir-diare',
-    },
-    description:
-        'My Dir - My little space on internet, place to share my thoughts and ideas',
-}
+  metadataBase: new URL('https://dir-diare.vercel.app'),
+  alternates: {
+    canonical: '/'
+  },
+  title: {
+    default: 'Dir-diare',
+    template: '%s | Dir-diare'
+  },
+  description: 'My Dir - My little space on internet, place to share my thoughts and ideas'
+};
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
 
 const playfair_display = Playfair_Display({
-    subsets: ['latin'],
-    weight: ['400', '700'],
-    variable: '--font-playfair-display',
-    display: 'swap',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-playfair-display',
+  display: 'swap',
 })
-const space_grotesk = Space_Grotesk({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-space-grotesk',
+const space_grotesk = Space_Grotesk({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-space-grotesk',
 })
 
 export default function RootLayout({
@@ -49,12 +48,12 @@ export default function RootLayout({
             )}
         >
             <body className="antialiased tracking-tight bg-white dark:bg-neutral-950 text-black dark:text-neutral-100">
-                <main className="max-w-2xl mb-40 flex flex-col">
-                    {children}
-                </main>
-                {/* <BackToTop /> */}
-                <SpeedInsights />
-                <Analytics />
+                    <main className="max-w-2xl mb-40 flex flex-col mx-4 mt-8 md:mx-auto">
+                        {children}
+                    </main>
+                    {/* <BackToTop /> */}
+                    <SpeedInsights />
+                    <Analytics />
             </body>
         </html>
     )
