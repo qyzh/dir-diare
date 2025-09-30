c89ae747c70ad8ae72bc49807bee25961de54630
qyzh <mrveranda@gmail.com>
2025-09-30 12:32:39 +0700

    fix: delete version and error on footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,24 +1,10 @@
 'use client'
 import Link from 'next/link'
 import { AnimatedBelow } from './animated-section'
-import { useState, useEffect } from 'react'
+
 
 const Footer = () => {
-    const [commitId, setCommitId] = useState<string | null>(null)
 
-    useEffect(() => {
-        const fetchCommitId = async () => {
-            try {
-                const response = await fetch('/api/latest-commit')
-                const data = await response.json()
-                setCommitId(data.commitId.substring(0, 8)) // Display first 8 characters
-            } catch (error) {
-                console.error('Failed to fetch commit ID:', error)
-            }
-        }
-
-        fetchCommitId()
-    }, [])
 
     return (
         <AnimatedBelow delay={1.5}>
@@ -31,19 +17,7 @@ const Footer = () => {
                         >
                             Dir-diare
                         </Link>{' '}
-                        {commitId && (
-                            <>
-                                {' '}
-                                <a
-                                    href={`https://github.com/qyzh/dir-diare/commit/${commitId}`}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="text-xs py-0.5 px-1.5 bg-neutral-300/10 border border-neutral-300/20 text-neutral-300 hover:bg-emerald-300/10 hover:border-emerald-300/20 hover:text-emerald-300 transition-colors duration-200 rounded"
-                                >
-                                    Commit: {commitId}
-                                </a>
-                            </>
-                        )}
+                        
                     </div>
                     <div>
                         <span className="italic">
