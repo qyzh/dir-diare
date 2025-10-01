5f775c9f3baf31d7dc012e1e87435f1ad3e899e9
qyzh <mrveranda@gmail.com>
2025-10-01 11:06:52 +0700

    style: update ui for w/ and l/

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,7 +18,18 @@ const PostCard = ({ post }: PostCardProps) => {
     return (
         <>
             <div key={post.slug}>
-                <span className="webtree">└──</span>
+                <span className="webtree">−rw−r−−r−−</span>{' '}
+                <span className="webtree uppercase">{post.author}</span>
+                <span className="webtree">
+                    {new Date(post.publishedAt)
+                        .toLocaleDateString('en-GB', {
+                            day: '2-digit',
+                            month: 'short',
+                            year: '2-digit',
+                            timeZone: 'Asia/Jakarta',
+                        })
+                        .toUpperCase()}
+                </span>
                 <Link
                     key={post.slug}
                     className="group text-lg transition-colors duration-200"
@@ -38,10 +49,10 @@ export async function BlogPosts() {
     return (
         <div role="list" aria-label="Blog posts" className="group">
             <div>
-                <span className="webtree">└──</span>
-                <span className="webmain">tulisan/</span>
+                <span className="webroot">web/dirdiare/app:</span>
+                <span className="webmain">ls -l tulisan</span>
             </div>
-            <div className="ml-6 ">
+            <div>
                 {allBlogs.map((post) => (
                     <PostCard key={post.slug} post={post} />
                 ))}
@@ -49,4 +60,3 @@ export async function BlogPosts() {
         </div>
     )
 }
-
