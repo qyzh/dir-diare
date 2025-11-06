0a6f53d911f686faea052ee345148cb6224938db
qyzh <mrveranda@gmail.com>
2025-11-06 14:41:35 +0700

    style: adjust for light mode

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import type { Metadata } from 'next'
-import { AnimatedZoom, AnimatedRight } from 'app/components/animated-section'
 import Breadcrumbs from 'app/components/breadcrumbs'
 import Footer from 'app/components/footer'
-import UKpathdir from 'app/components/ukpathdir'
 
 export const metadata: Metadata = {
     title: 'About',
@@ -43,7 +41,9 @@ const EduTimelineItem: React.FC<Edu> = ({ title, desc, year, place }) => (
         <time className="text-xs tracking-wide text-neutral-500">{year}</time>
         <h3 className="text-md tracking-wide">
             <span className="font-semibold pr-1">{desc}</span>
-            <span className="font-mono text-neutral-400">at</span>
+            <span className="font-mono text-neutral-800 text-neutral-300/70">
+                at
+            </span>
             <span className="font-semibold pl-1">{title}</span>
         </h3>
         <p className="text-neutral-500 font-mono text-sm">{place}</p>
@@ -51,67 +51,71 @@ const EduTimelineItem: React.FC<Edu> = ({ title, desc, year, place }) => (
 )
 
 const EduTL: React.FC = () => (
-    <AnimatedZoom delay={0.5} className="grid grid-cols-1 gap-2">
+    <div className="grid grid-cols-1 gap-2">
         <div id="edu" className="relative space-y-6">
             <div className="relative px-4 space-y-8 before:absolute before:top-2 before:bottom-0 before:w-0.5 before:bg-neutral-900">
                 {eduData.map((edu, index) => (
-                    <AnimatedRight key={edu.title} delay={0.8 + index * 0.2}>
+                    <div key={edu.title} delay={0.8 + index * 0.2}>
                         <EduTimelineItem {...edu} />
-                    </AnimatedRight>
+                    </div>
                 ))}
             </div>
         </div>
-    </AnimatedZoom>
+    </div>
 )
 
 const DearDairy: React.FC = () => (
-    <AnimatedZoom delay={0.8}>
-        <div id="dear-diary">
-            <strong>Why the name dir-diare?</strong>
-            <p className="text-neutral-300/70">
-                It started with the classic
-                <i className=" text-white/90"> dear diary </i>
-                idea. Sweet, right? But then I thought—nah, too plain. Needs
-                more nerd... and maybe a bit more toilet humour.
-            </p>
-            <p>
-                So came <strong>dir-diare</strong>:
-            </p>
-            <ul className="list-disc pl-6 text-neutral-300/70">
-                <li>
-                    <strong className="font-mono text-white">"dir" </strong>
+    <div id="dear-diary">
+        <strong>Why the name dir-diare?</strong>
+        <p className="text-neutral-800 dark:text-neutral-300/70">
+            It started with the classic
+            <i className="text-black/90 dark:text-white/90"> dear diary </i>
+            idea. Sweet, right? But then I thought—nah, too plain. Needs more
+            nerd... and maybe a bit more toilet humour.
+        </p>
+        <p>
+            So came <strong>dir-diare</strong>:
+        </p>
+        <ul className="list-disc pl-6  text-neutral-800  text-neutral-300/70">
+            <li>
+                <p className="text-neutral-800 dark:text-neutral-300/70">
+                    <strong className="font-mono text-black dark:text-white">
+                        "dir"{' '}
+                    </strong>
                     is short for {'  '}
-                    <span className="font-mono bg-white/5 text-neutral-100">
+                    <span className="font-mono bg-black/5  dark:bg-white/5 text-neutral-600 dark:text-neutral-300">
                         directory
                     </span>
                     {'  '} in programming—where we dump files. In this case,
                     it's where I dump thoughts, rants, ideas, and brain-farts.
-                </li>
-                <li>
-                    <strong className="text-white">"diare" </strong>(a cheeky
-                    twist on "diary") literally means {'  '}
+                </p>
+            </li>
+            <li>
+                <p className="text-neutral-800 dark:text-neutral-300/70">
+                    <strong className="text-black dark:text-white">
+                        "diare"{' '}
+                    </strong>
+                    (a cheeky twist on "diary") literally means {'  '}
                     <i>diarrhoea</i>
                     {'  '} in Indonesian. Which, fun fact: I actually get if I
                     drink milk. But also, it perfectly captures how my thoughts
                     sometimes rush out in... uncontrollable bursts. 💻💩
-                </li>
-            </ul>
-            <p className="text-neutral-300/70">
-                So yeah, this blog is basically my mental dump folder—raw,
-                weird, and sometimes mildly alarming. Read at your own risk, and
-                maybe bring some digital tissues.
-            </p>
-        </div>
-    </AnimatedZoom>
+                </p>
+            </li>
+        </ul>
+        <p className="text-neutral-800 dark:text-neutral-300/70">
+            So yeah, this blog is basically my mental dump folder—raw, weird,
+            and sometimes mildly alarming. Read at your own risk, and maybe
+            bring some digital tissues.
+        </p>
+    </div>
 )
 
 const AboutPage: React.FC = () => (
     <section>
         <Breadcrumbs />
         <main>
-            <UKpathdir name="deardiary" type="md" />
             <DearDairy />
-            <UKpathdir name="edu" type="md" />
             <EduTL />
         </main>
         <Footer />
