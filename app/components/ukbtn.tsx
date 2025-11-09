02f7f922d1ef54b5fd30c66703a2a3dc99f9d700
qyzh <mrveranda@gmail.com>
2025-11-10 00:56:19 +0700

    style: update color UI

--- app/components/ukbtn.tsx
+++ app/components/ukbtn.tsx
@@ -1,51 +1,55 @@
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEventHandler } from 'react'
 
 interface UKButtonProps {
-  variant?: 'primary' | 'secondary' | 'outline' | 'error' ;
-  size?: 'sm' | 'md' | 'lg';
-  className?: string;
-  children: React.ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-  disabled?: boolean;
-  type?: 'button' | 'submit' | 'reset';
+    variant?: 'primary' | 'secondary' | 'outline' | 'error'
+    size?: 'sm' | 'md' | 'lg'
+    className?: string
+    children: React.ReactNode
+    onClick?: MouseEventHandler<HTMLButtonElement>
+    disabled?: boolean
+    type?: 'button' | 'submit' | 'reset'
 }
 
 const UKButton: React.FC<UKButtonProps> = ({
-  variant = 'primary',
-  size = 'md',
-  className = '',
-  children,
-  onClick,
-  disabled = false,
-  type = 'button',
-  ...props
+    variant = 'primary',
+    size = 'md',
+    className = '',
+    children,
+    onClick,
+    disabled = false,
+    type = 'button',
+    ...props
 }) => {
-  const baseStyles = "inline-flex items-center justify-center font-medium border transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
-  
-  const variants = {
-    primary: 'bg-teal-600 text-white hover:bg-teal-700 dark:border-teal-600 dark:bg-teal-800 dark:hover:bg-teal-600',
-    secondary: 'bg-gray-100 text-gray-900 hover:bg-gray-200 dark:border-neutral-700 dark:hover:border-neutral-600 dark:bg-white/5 dark:text-neutral-100 dark:hover:bg-neutral-800',
-    outline: 'border border-gray-300 bg-transparent hover:bg-gray-100 dark:border-neutral-700 dark:hover:bg-neutral-800',
-    error: 'bg-rose-600 text-gray-900 hover:bg-gray-100 dark:border-rose-600  dark:bg-rose-800 dark:text-rose-300 dark:hover:border-rose-700 dark:hover:bg-rose-950'
-  };
-  
-  const sizes = {
-    sm: 'h-8 px-3 text-sm',
-    md: 'h-10 px-4',
-    lg: 'h-12 px-6 text-lg'
-  };
-  
-  return (
-    <button
-      type={type}
-      className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`}
-      onClick={onClick}
-      disabled={disabled}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+    const baseStyles =
+        'inline-flex items-center justify-center font-medium border transition-colors hover:cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
 
-export default UKButton;
+    const variants = {
+        primary:
+            'bg-teal-500 border-teal-400 text-teal-100 hover:bg-teal-600 dark:border-teal-600 dark:bg-teal-800 dark:hover:bg-teal-600',
+        secondary:
+            'bg-gray-200 border-gray-300 text-gray-700 hover:bg-gray-300 hover:text-gray-500 dark:border-neutral-700 dark:hover:border-neutral-600 dark:bg-white/5 dark:text-neutral-100 dark:hover:bg-neutral-800',
+        outline:
+            'border border-gray-300 text-gray-500 bg-transparent hover:bg-gray-100 hover:text-gray-700 dark:border-neutral-700 dark:hover:bg-neutral-800 dark:hover:text-neutral-300',
+        error: 'bg-rose-500 text-rose-200 border-rose-600 hover:bg-rose-700 dark:border-rose-600  dark:bg-rose-800 dark:text-rose-300 dark:hover:border-rose-700 dark:hover:bg-rose-950',
+    }
+
+    const sizes = {
+        sm: 'h-8 px-3 text-sm',
+        md: 'h-10 px-4',
+        lg: 'h-12 px-6 text-lg',
+    }
+
+    return (
+        <button
+            type={type}
+            className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`}
+            onClick={onClick}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default UKButton
