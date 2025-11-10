a49d7a416d5147e338325225aa6a3f9e9b2ea84e
qyzh <mrveranda@gmail.com>
2025-11-10 14:33:37 +0700

    style: fix scroll on drawer

--- app/components/ukdrawer.tsx
+++ app/components/ukdrawer.tsx
@@ -20,8 +20,8 @@ export default function UKDrawer({
             <Drawer.Portal>
                 <Drawer.Overlay className="fixed inset-0 bg-black/40" />
                 <Drawer.Content className="bg-neutral-900 fixed right-0 top-0 bottom-0 w-[310px] outline-none">
-                    <div className="p-4 h-full">
-                        <div className="max-w-md">
+                    <div className="p-4 h-full flex flex-col">
+                        <div className="max-w-md h-full flex flex-col">
                             <Drawer.Close className=" text-neutral-500  text-neutral-400 hover:text-neutral-100 transition-colors">
                                 <span className="sr-only">Close</span>
                                 <PanelRightClose className="w-6 h-6" />
