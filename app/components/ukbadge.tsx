1343d8494dda29cc2ead625b29a2cf7f3593ff9f
qyzh <mrveranda@gmail.com>
2025-11-10 21:43:33 +0700

    style: update UI badge

--- app/components/ukbadge.tsx
+++ app/components/ukbadge.tsx
@@ -30,11 +30,11 @@ export const Badge = ({
             'bg-gray-200 text-gray-700 border-gray-300 hover:bg-gray-400',
         outline:
             'bg-transparent text-neutral-800 border-neutral-300  dark:border-neutral-700 dark:text-neutral-50',
-        dev: 'bg-purple-950/80 text-purple-500 border-purple-700 hover:border-purple-600 hover:bg-purple-900 hover:text-purple-400',
+        dev: 'bg-purple-950/80 text-purple-300 dark:text-purple-500 border-purple-700 hover:border-purple-600 hover:bg-purple-900 hover:text-purple-400',
         running:
             'bg-orange-950/80 text-orange-500 border-orange-700 hover:border-orange-600 hover:bg-orange-900 hover:text-orange-400',
         gamer: 'bg-sky-950/80 text-sky-500 border-sky-700 hover:border-sky-600 hover:bg-sky-900 hover:text-sky-400',
-        muted: 'bg-neutral-100/50 text-neutral-400 border-neutral-200/50 hover:bg-neutral-100/70',
+        muted: 'bg-gray-200 border-gray-300 text-gray-800 opacity-80 dark:bg-neutral-800 dark:text-neutral-300 dark:border-neutral-200/50 dark:hover:bg-neutral-100/70',
     }
 
     return (
@@ -44,7 +44,7 @@ export const Badge = ({
             px-2 py-1 text-xs font-medium
             border transition-colors
             ${variants[variant]}
-            ${disabled ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''}
+            ${disabled ? 'opacity-60 cursor-not-allowed pointer-events-none' : ''}
             ${className}
         `}
         >
