5ee43e2ac239934aa9784a8de6ae78768f62f921
qyzh <mrveranda@gmail.com>
2025-11-13 11:40:24 +0700

    feat(footer): add about-page link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
                 <div className="flex justify-between items-center gap-2 font-mono text-sm text-neutral-500 dark:text-neutral-400">
                     <div>
                         <Link
-                            href="/"
+                            href="/about"
                             className="text-neutral-700 dark:text-neutral-200 underline hover:text-emerald-600 dark:hover:text-emerald-300 transition-colors duration-200"
                         >
                             Dir-diare
