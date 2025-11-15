fe6027ed0387148d42e48833f6d5a6219d41ccb7
qyzh <mrveranda@gmail.com>
2025-11-15 14:55:22 +0700

    style: change UI color

--- app/components/breadcrumbs.tsx
+++ app/components/breadcrumbs.tsx
@@ -21,11 +21,11 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ post }) => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.7, ease: 'easeOut' }}
-                className="inline-flex truncate overflow-hidden font-mono text-neutral-500 items-center py-1.5"
+                className="px-1.5 inline-flex truncate overflow-hidden font-mono text-neutral-700 hover:text-neutral-100 items-center bg-neutral-200 hover:bg-neutral-800 dark:bg-neutral-900 dark:hover:bg-neutral-600 rounded  transition-colors duration-300"
             >
                 <Link
                     href="/"
-                    className="whitespace-nowrap px-1 hover:text-neutral-300 text-sm transition-colors"
+                    className="whitespace-nowrap px-1 hover:text-teal-600 dark:text-neutral-300 text-sm"
                 >
                     ~
                 </Link>
@@ -35,16 +35,13 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ post }) => {
                             /
                         </div>
                         {index === paths.length - 1 ? (
-                            <div className="text-blue-600 dark:text-sky-300 px-1 text-sm">
+                            <div className="dark:text-sky-300 px-1 text-sm">
                                 {currentTitle}{' '}
-                                <span className="animate-pulse text-teal-600 dark:text-yellow-400">
-                                    |
-                                </span>
                             </div>
                         ) : (
                             <Link
                                 href={`/${paths.slice(0, index + 1).join('/')}`}
-                                className="px-1 text-neutral-500 hover:text-teal-600 text-sm transition-colors"
+                                className="px-1 dark:text-teal-300 hover:text-teal-600 text-sm transition-colors"
                             >
                                 {path}
                             </Link>
